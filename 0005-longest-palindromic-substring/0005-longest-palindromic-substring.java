3cbe93e162025eda6f79e4b38a1bea2c20d8059b
LOKESH PALLAPU <lokeshpallapucse@gmail.com>
2026-02-02 11:24:12 +0530

    Time: 16 ms (54.71%), Space: 43.8 MB (58.42%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        if (n < 2) return s;
+
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < n; i++) {
+            int l = i, r = i;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+
+            l = i;
+            r = i + 1;
+            while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+                if (r - l + 1 > maxLen) {
+                    start = l;
+                    maxLen = r - l + 1;
+                }
+                l--;
+                r++;
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+}
